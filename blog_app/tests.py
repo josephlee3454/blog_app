16d9adf728f95972793e9fc6cc671896d622145d
josephlee3454 <joseph.lee3454@gmail.com>
2020-08-27 23:26:56 -0700

    added README

--- blog_app/tests.py
+++ blog_app/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
-
+from django.test import SimpleTestCase
+from django.urls import reverse
 # Create your tests here.
+
+class BlogTest(SimpleTestCase):
+  def helper_status_code_200(self, url_name):
+    url = reverse(url_name)
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+
+
+  def test_home_page_status(self):
+    self.helper_status_code_200('home')
--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,5 +3,5 @@ from .views import HomePageView, PostDetailView
 
 urlpatterns = [
   path('', HomePageView.as_view(), name='home'),
-  path('blog/<int:pk>', PostDetailView.as_view(), name='blog'),
+  path('post-detail/<int:pk>', PostDetailView.as_view(), name='post-detail'),
 ]
